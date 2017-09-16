5770c7c33e984667515ba6ee1f41b3a536f17491
Sumin Go <jennifer045020@gmail.com>
2017-09-16 21:01:20 +0900

    Build Constructor

--- HubGaru/app/src/main/java/teampj/java/dsm/hubgaruandroid/Network/Interceptor/AddCookiesInterceptor.java
+++ HubGaru/app/src/main/java/teampj/java/dsm/hubgaruandroid/Network/Interceptor/AddCookiesInterceptor.java
@@ -1,15 +1,24 @@
 package teampj.java.dsm.hubgaruandroid.Network.Interceptor;
 
+import android.content.Context;
+
 import java.io.IOException;
 
 import okhttp3.Interceptor;
 import okhttp3.Response;
+import teampj.java.dsm.hubgaruandroid.Network.Preference.CookieSharedPreferences;
 
 /**
  * Created by user on 2017-09-15.
  */
 
 public class AddCookiesInterceptor implements Interceptor{
+
+    CookieSharedPreferences cookieSharedPreferences;
+
+    public AddCookiesInterceptor (Context context) {
+        cookieSharedPreferences = CookieSharedPreferences.getInstanceOf(context);
+    }
 
     @Override
     public Response intercept(Chain chain) throws IOException {
