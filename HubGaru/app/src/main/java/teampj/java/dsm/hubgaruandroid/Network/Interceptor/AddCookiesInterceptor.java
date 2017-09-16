27f519706a0fea7d30864115a7ea104e8bbc2ec6
Sumin Go <jennifer045020@gmail.com>
2017-09-16 20:46:21 +0900

    Make SharedCookie Class

---
+++ HubGaru/app/src/main/java/teampj/java/dsm/hubgaruandroid/Network/Interceptor/AddCookiesInterceptor.java
@@ -0,0 +1,19 @@
+package teampj.java.dsm.hubgaruandroid.Network.Interceptor;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Response;
+
+/**
+ * Created by user on 2017-09-15.
+ */
+
+public class AddCookiesInterceptor implements Interceptor{
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        return null;
+    }
+
+}
